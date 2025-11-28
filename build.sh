88698a40a2a191070c7299c98a088e0b8a5e7e03
Iconia7 <mwanginewton239@gmail.com>
2025-11-28 18:57:15 +0300

    Deploy final (Secured)

--- build.sh
+++ build.sh
@@ -2,6 +2,12 @@
 # exit on error
 set -o errexit
 
+# 1. Upgrade pip first (Fixes many version errors)
+pip install --upgrade pip
+
+# 2. Install your requirements
 pip install -r requirements.txt
+
+# 3. Django setup
 python manage.py collectstatic --no-input
 python manage.py migrate

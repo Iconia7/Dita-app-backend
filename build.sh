5d7ddc45593381c52a055b8f810a172fdb7b51cb
Iconia7 <mwanginewton239@gmail.com>
2025-11-28 20:13:20 +0300

    superuser

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 # exit on error
 set -o errexit
 
-# 1. Upgrade pip first (Fixes many version errors)
-pip install --upgrade pip
-
-# 2. Install your requirements
 pip install -r requirements.txt
 
-# 3. Django setup
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# --- ADD THIS LINE BELOW ---
+# This tries to create the user using the Env Vars you set in Step 1.
+# The '|| true' part ensures the build doesn't fail if the user already exists.
+python manage.py createsuperuser --noinput || true

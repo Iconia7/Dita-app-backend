83685d6e970569c8da499f169fce023e8aa85d54
Iconia7 <mwanginewton239@gmail.com>
2025-12-06 12:55:15 +0300

    build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py migrate
 
 # --- ADD THIS LINE BELOW ---
 # This tries to create the user using the Env Vars you set in Step 1.

3ce3453b9949a1b5dc883822742d8bf07acd2fd2
Iconia7 <mwanginewton239@gmail.com>
2025-12-07 09:45:27 +0300

    cloudinary saving

--- dita_backend/settings.py
+++ dita_backend/settings.py
@@ -36,14 +36,14 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'cloudinary_storage',
+    'cloudinary',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cloudinary_storage',
-    'cloudinary',
     'anymail',
     'import_export',
     'rest_framework',

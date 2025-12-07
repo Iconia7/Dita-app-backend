38bd04fa674547b3150bfb983ab896ba81c1bd81
Iconia7 <mwanginewton239@gmail.com>
2025-12-07 11:30:46 +0300

    fixed

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input --clear
 
 # --- ADD THIS LINE BELOW ---
 # This tries to create the user using the Env Vars you set in Step 1.

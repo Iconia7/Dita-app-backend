d2ef1fb655fcd1e8e0d41036145691641e17993c
Iconia7 <mwanginewton239@gmail.com>
2025-12-07 15:56:20 +0300

    Revert to WhiteNoise compression

--- dita_backend/settings.py
+++ dita_backend/settings.py
@@ -182,14 +182,12 @@ STORAGES = {
     "default": {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
-    # TEMPORARY FIX: Use standard Django storage
     "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
-# TEMPORARY FIX:
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
